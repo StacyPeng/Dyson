4f3dfc562e41e71ece6f8800fcccbc46fafb4c79
Stacy <C.Peng6@newcastle.ac.uk>
2024-04-23 17:48:01 +0100

    注释掉所有恒乾的代码
    不注释恒乾的代码主程序无法运行，问题出在JPA框架上

--- src/main/java/team11/chengyu/mapper/CourseRepository.java
+++ src/main/java/team11/chengyu/mapper/CourseRepository.java
@@ -1,12 +1,14 @@
+/*
 //Auther：Hengqian Mao
 package team11.chengyu.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Repository;
 import team11.chengyu.domian.Course;
 
-@Mapper
 @Repository
 public interface CourseRepository extends JpaRepository<Course, Long> {
 }
+*/
--- src/test/java/team11/chengyu/DysonApplicationTests.java
+++ src/test/java/team11/chengyu/DysonApplicationTests.java
@@ -1,9 +1,6 @@
 package team11.chengyu;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import team11.chengyu.service.impl.CourseService;
 
 @SpringBootTest
 class DysonApplicationTests {
