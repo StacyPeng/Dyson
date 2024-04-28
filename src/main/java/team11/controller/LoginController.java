ce5cd1c5fc38e423aec75cff124764e8aa7aa948
Stacy <C.Peng6@newcastle.ac.uk>
2024-04-28 12:05:08 +0100

    1111

--- src/main/java/team11/controller/LoginController.java
+++ src/main/java/team11/controller/LoginController.java
@@ -1,12 +1,13 @@
 package team11.controller;
 
-import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import team11.mapper.StudentMapper;
+
+import javax.servlet.http.HttpSession;
 
 
 /**
