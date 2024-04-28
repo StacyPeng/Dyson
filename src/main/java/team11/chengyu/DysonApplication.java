fb6dd627f3bf047379c09e62dac8906afa1c9e30
Stacy <C.Peng6@newcastle.ac.uk>
2024-04-28 13:02:26 +0100

    能不能回周四啊

--- src/main/java/team11/chengyu/DysonApplication.java
+++ src/main/java/team11/chengyu/DysonApplication.java
@@ -3,10 +3,12 @@ package team11.chengyu;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = "team11.repository")  // JPA 仓库的新位置
+@EntityScan("team11.domian")
+@EnableJpaRepositories(basePackages = "team11.repository")
 @MapperScan({"team11.mapper"})
 public class DysonApplication{
 
